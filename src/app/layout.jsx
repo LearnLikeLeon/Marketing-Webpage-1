95eb5d9dcebcad4657a3a95aedf420bf62454807
LearnLikeLeon <leon@learnlikeleon.com>
2025-03-09 13:01:00 -0400

    ads verification

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,9 +23,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-1126149056959231"
+      ></meta>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1126149056959231"
+          crossOrigin="anonymous"
+        ></Script>
+
         {children}
       </body>
     </html>
