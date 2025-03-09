382d4d803d69ef1c24927f03c1854488cbe7db87
LearnLikeLeon <leon@learnlikeleon.com>
2025-03-09 13:30:19 -0400

    ads verification_3

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -30,13 +30,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Script
-          async
-          // src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1126149056959231"
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
-          crossOrigin="anonymous"
-        ></Script>
-
         {children}
       </body>
     </html>
